e653e32caa1c3e375ed910c1d3180f2bd8a7230d
ajosvargas <alejandrojosvargas@gmail.com>
2024-03-24 11:03:30 -0600

    init commit

---
+++ script.js
@@ -0,0 +1,110 @@
+const board = document.getElementById('gameboard');
+let score = 0;
+let compareArr = [];
+
+
+const pairs = [
+    'r',
+    'r',
+    'b',
+    'b',
+    'y',
+    'y',
+    'o',
+    'o',
+    'g',
+    'g',
+    'p',
+    'p',
+    'h',
+    'h',
+    'c',
+    'c',
+    'l',
+    'l',
+]
+
+board.addEventListener('click', gameLogic);
+
+function gameLogic (e) {
+    compareArr.push(e.srcElement);
+    e.srcElement.classList.add(e.srcElement.getAttribute('data-color'));
+    if(compareArr.length === 2){
+        board.removeEventListener('click', gameLogic);
+        checkForPairs();
+        setTimeout(()=> {
+            checkforWin();
+            compareArr = [];
+            board.addEventListener('click', gameLogic);
+        },1000)
+    }
+    score++;
+}
+
+function checkforWin() {
+    let pieces = [...board.children];
+    let win = 0;
+
+    pieces.forEach((piece)=>{
+        if(piece.getAttribute('data-pair') === 'true') {
+            win++;
+        }
+    })
+
+    if(win === pieces.length){
+        pieces.forEach((piece)=>{
+            piece.remove();
+        })
+        let message = document.createElement('p');
+        let moves = document.createElement('p');
+        message.innerText = 'YOU WON!!!';
+        moves.innerText = `It Took You: ${score} Moves`;
+        document.body.append(message);
+        document.body.append(moves);
+    }
+}
+
+function checkForPairs() {
+    if(compareArr[0].getAttribute('data-color') === compareArr[1].getAttribute('data-color') && compareArr[0] !== compareArr[1]){
+        compareArr[0].setAttribute('data-pair','true');
+        compareArr[1].setAttribute('data-pair','true');
+    } else if(compareArr[0] === compareArr[1] && score !== 1) {
+        compareArr[0].classList.remove(compareArr[0].getAttribute('data-color'));
+        compareArr[1].classList.remove(compareArr[1].getAttribute('data-color'));
+        score--;
+        console.log(score);
+    }
+    else {
+        setTimeout(()=> {
+            compareArr[0].classList.remove(compareArr[0].getAttribute('data-color'));
+            compareArr[1].classList.remove(compareArr[1].getAttribute('data-color'));
+        },1000)
+    }
+}
+
+function startGame() {
+    document.getElementById('start').remove();
+    shuffleBoard(pairs);
+}
+
+function shuffleBoard(arr){
+    for(let i = 0; i < arr.length; i++){
+        let tempVal = arr[i];
+        // getting random value to switch current iterator with
+        let randomVal = Math.floor(Math.random() * arr.length);
+
+        arr[i] = arr[randomVal];
+        arr[randomVal] = tempVal; 
+    }
+    createBoardItems(arr);
+}
+
+function createBoardItems (arr) {
+    arr.forEach((color) => {
+        let boardItem = document.createElement('div');
+        boardItem.classList.add('square');
+        boardItem.setAttribute('data-pair', 'false');
+        boardItem.setAttribute('data-color', `${color}`);
+        board.append(boardItem);
+    })
+}
